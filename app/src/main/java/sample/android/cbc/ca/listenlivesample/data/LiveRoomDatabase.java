e283dfbe6c8ffb2fafc3be9ecf5fc3844f816e24
Sean Nguyen <sean.nguyen@cbc.ca>
2018-05-03 11:01:19 -0400

    DB init

---
+++ app/src/main/java/sample/android/cbc/ca/listenlivesample/data/LiveRoomDatabase.java
@@ -0,0 +1,44 @@
+package sample.android.cbc.ca.listenlivesample.data;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+/* *****  LIST ENTITIES (DB TABLES) HERE  ***** */
+@Database(entities = {}, version = 1)
+public abstract class LiveRoomDatabase extends RoomDatabase {
+
+    private static LiveRoomDatabase sInstance;
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onOpen(@NonNull SupportSQLiteDatabase db) {
+            super.onOpen(db);
+
+
+        }
+    };
+
+    public static LiveRoomDatabase getDatabase(final Context context) {
+        if (sInstance == null) {
+            synchronized (LiveRoomDatabase.class) {
+                if (sInstance == null) {
+                    //Create DB
+                    sInstance = Room.databaseBuilder(context.getApplicationContext(),
+                            LiveRoomDatabase.class, "listen_database")
+                                .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+
+        return sInstance;
+    }
+
+    /*
+    Define DAOs here
+     */
+}
